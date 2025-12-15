86fad33708b71cc3ab08bd81628b4d2ff7d0ec70
diogobarros02 <diogonbarros2002@gmail.com>
2025-12-15 16:24:39 +0000

    test changes

--- tests/assert_under_10_percent_null.sql
+++ tests/assert_under_10_percent_null.sql
@@ -3,5 +3,3 @@ select
 from {{ ref('my_first_dbt_model') }}
 having percent_nulls > 40
 
-SELECT sum(c_acctbal) from {{ ref('playing_with_tests') }}  as total_acctbal
-having total_acctbal > 100000000
