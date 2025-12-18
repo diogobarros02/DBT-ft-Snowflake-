176efb3eb3684f78a4ddb537f714e824992607f4
diogobarros02 <diogonbarros2002@gmail.com>
2025-12-18 10:07:21 +0000

    test fixes

--- models/example/playing_with_tests.sql
+++ models/example/playing_with_tests.sql
@@ -1,7 +1,8 @@
 with sample_customer as (
-    select * from {{source('sample', 'customer')}}
+    select * from {{source('sample2', 'customer')}}
 )
 
 select c_custkey, 
 c_mktsegment, 
-{{rename_segments('c_mktsegment')}}  as mkt_segment_adjusted from sample_customer  
+{{rename_segments('c_mktsegment')}} as mkt_segment_adjusted,
+c_acctbal  from sample_customer,
